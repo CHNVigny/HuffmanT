46d3646083d1b606a79af214f54afb2a31d7709c
Wang Hejun <945196788@qq.com>
2015-12-22 20:45:13 +0800

    V 1.0
    还与很多bug

--- main2.cpp
+++ main2.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <malloc.h>
 #include <string.h>
+#include <string>
 #define MAXVALUE  10000
 #define N 128 //本程序只对ascii码表中出现的字符编码。
 #define MAXBIT 128
@@ -22,20 +23,23 @@ typedef struct node1
 typedef struct node2
 {
 	char bit[128];
+	char value;
 }*HuffmanCode, HCNode;
 
-void strRev(char *s)//用于字符串反转
+char* strRev(const char* str)
 {
-	char temp, *endchar;
-	endchar = s + strlen(s) - 1;
-	while (endchar > s)
+	size_t len = strlen(str);
+	char* pTemp = new char[len + 1];
+	memset(pTemp, 0, len + 1);
+	memcpy(pTemp, str, len);
+	char ch;
+	for (size_t i = 0;i<len / 2;i++)
 	{
-		temp = *s;
-		*s = *endchar;
-		*endchar = temp;
-		--endchar;
-		++s;
+		ch = pTemp[i];
+		pTemp[i] = pTemp[len - 1 - i];
+		pTemp[len - 1 - i] = ch;
 	}
+	return pTemp;
 }
 
 int count_not_zero(int *data, int n)//用于求数组中不为零的个数
@@ -53,26 +57,23 @@ int count_not_zero(int *data, int n)//用于求数组中不为零的个数
 
 void Select(HuffmanTree HT, int n, int *x1, int *x2)	//在HT[k](1 <= k <= m)中选择两个双亲为0且权值最小的结点，记下标为s1，s2，n=N
 {
-	int i, j;
+	int j;
 	int m1, m2;
-	for (i = 0; i < n; i++)
+	m1 = m2 = MAXVALUE;     /* m1、m2中存放两个无父结点且结点权值最小的两个结点 */
+							/* 找出所有结点中权值最小、无父结点的两个结点，并合并之为一颗二叉树 */
+	for (j = 0; j < n; j++)
 	{
-		m1 = m2 = MAXVALUE;     /* m1、m2中存放两个无父结点且结点权值最小的两个结点 */
-								/* 找出所有结点中权值最小、无父结点的两个结点，并合并之为一颗二叉树 */
-		for (j = 0; j < n + i; j++)
+		if (HT[j].weight < m1 && HT[j].parent == 0)
 		{
-			if (HT[j].weight < m1 && HT[j].parent == 0)
-			{
-				m2 = m1;
-				x2 = x1;
-				m1 = HT[j].weight;
-				*x1 = j;
-			}
-			else if (HT[j].weight < m2 && HT[j].parent == 0)
-			{
-				m2 = HT[j].weight;
-				*x2 = j;
-			}
+			m2 = m1;
+			//x2 = x1;
+			m1 = HT[j].weight;
+			*x1 = j;
+		}
+		else if (HT[j].weight < m2 && HT[j].parent == 0)
+		{
+			m2 = HT[j].weight;
+			*x2 = j;
 		}
 	}
 }
@@ -95,7 +96,7 @@ void count(int *data)//data是一个数组，每个元素的下标代表字符的ascii码
 }
 /*2.依此文本出现的字符和出现的频次，建立该文本字符集的哈夫曼编码表，以文件形式存储并显示。 */
 //（1）我们先建立一个哈弗曼树，我们在这里运用排序的方法找到两个权值最小的点，然后生成新的点并且删除之前的两个点，然后再对新的点集进行排序。
-void CreatHuffmanTree(HuffmanTree HT, int n, int *data)
+void CreatHuffmanTree(HuffmanTree HT, int n, int *data)//这里的n代表共统计出了n个字符。
 {
 	int i;
 	int j = 0;
@@ -107,7 +108,7 @@ void CreatHuffmanTree(HuffmanTree HT, int n, int *data)
 		HT[i].rch = 0;
 		//HT[j].weight = MAXVALUE;
 	}
-	for (i = 0;i < n;i++)
+	for (i = 0;i < N;i++)
 	{
 		if (data[i])
 		{
@@ -131,71 +132,112 @@ void CreatHuffmanTree(HuffmanTree HT, int n, int *data)
 
 //（2）在建立完哈弗曼树之后我们就可以生成哈弗曼编码了，过程是已建立的哈夫曼树中，从叶结点开始，沿结点的双亲链域回退到根结点，每回退一步，就走过了哈夫曼树的一个分支，从而得到一位哈夫曼码值，由于一个字符的哈夫曼编码是从根结点到相应叶结点所经过的路径上各分支所组成的0，1序列，因此先得到的分支代码为所求编码的低位码，后得到的分支代码为所求编码的高位码：
 
-void CreatHuffmanCode(HuffmanTree HT, HuffmanCode HC, int n)//这里的n代表哈夫曼码的位数，应该和HC.bit的位数一样多。128个
+void CreatHuffmanCode(HuffmanTree HT, HuffmanCode HC, int n)//n代表存了字符的HT的个数
 {
 	//char cd[n];
 	int start, c, i;
 	int f;
-	char *cd;
-	cd = (char*)malloc(n*sizeof(char));
-	cd[n - 1] = '\0';
-	for (i = 0;i < n; i++)
+	char *cd1, *cd2;
+	cd1 = (char*)malloc(n*sizeof(char));
+	cd2 = (char*)malloc(n*sizeof(char));
+	//cd[n - 1] = '\0';
+	for (i = 0;i < n; i++)				//HT的前j个元素里面存了字符。
 	{
-		start = n - 1;                 //start开始时指向最后，即编码结束的位置
+		start = 0;                 //start开始时指向最前
 		c = i;
-		f = HT[i].parent;                //f指向c的双亲结点
+		f = HT[c].parent;                //f指向c的双亲结点
 		while (f != 0)                     //从叶子开始向上回溯，直到根结点
-		{
-			--start;                     //回溯一次start指向前一个位置
+		{                   
 			if (HT[f].lch == c)
-				cd[start] = '0';             //c是f的左孩子，则生成代码‘0’
+				cd1[start] = '0';             //c是f的左孩子，则生成代码‘0’
 			else
-				cd[start] = '1';                 //c是f的右孩子，则生成代码‘1‘
+				cd1[start] = '1';                 //c是f的右孩子，则生成代码‘1‘
 			c = f;                                 //  继续向上回溯
-			f = HT[f].parent;
+			f = HT[c].parent;
+			start++;
+		}
+		cd1[start] = '\0';
+		cout << cd1 << endl;
+		cd2 = strRev(cd1);
+		cout << cd2 << endl;
+		for (int m = 0;m < 128;m++)
+		{
+			HC[i].bit[m] = -51;
 		}
 		//HC[i].bit = new char[n - start];               //为第i个字符编码分配空间
-		strcpy_s(HC[i].bit, 128, &cd[start]);                 //将求得的编码从临时空间cd复制到HC的当前行中
+		strcpy_s(HC[i].bit, 128, cd2);//将求得的编码从临时空间cd复制到HC的当前行中
+		HC[i].value = (char)HT[i].value;
 	}
-	delete cd;
+	free(cd1);
+	free(cd2);
 	ofstream fout;
 	fout.open("HTree.txt");
 	for (i = 0;i < n;i++)
 	{
-		char ch = i;
+		char ch = HC[i].value;
 		fout << ch << "\t" << HC[i].bit << endl;//以文本文件的形式显示
 	}
 	fout.close();
 }
 
-/*3.给出该字符集的任意文本文件的哈夫曼编码文件。
-在上一步已经实现。*/
-//4.对哈夫曼编码文件进行译码。 
-void TranslateHuffmanCode(HuffmanTree HT, HCNode HC, int n)
+/*3.给出该字符集的任意文本文件的哈夫曼编码文件。*/
+void Tranlate_To_HuffmanCode_File(HuffmanCode HC, int n)//n表示HC节点个数
 {
-	int f;
+	char w;
+	ifstream fin;
+	fin.open("source.txt", ios::in);
+	ofstream fout;
+	fout.open("result.txt");
+	while (!fin.eof())                         //循环读入直至文件末尾                           
+	{
+		fin.get(w);								//读入字符到w中
+		for (int i = 0;i < n;i++)
+		{
+			if (HC[i].value == w)
+			{
+				fout << HC[i].bit;
+				break;
+			}
+		}
+	}
+	fin.close();
+	fout.close();
+}
+//4.对哈夫曼编码文件进行译码。 
+void TranslateHuffmanCode(HuffmanTree HT, HuffmanCode HC, int n)//n表示 HT节点的个数。
+{
+	int f, j=0, k;//j记录所有节点的祖先的位置，k记录祖先节点的个数。
 	char c;
 	ifstream fin;
-	fin.open("CodeFile.txt");
+	fin.open("result.txt");
 	ofstream fout;
-	fout.open("RecieveFile.txt");
-	f = 2 * n - 1;
+	fout.open("translation.txt");
+	for (int i = 0;i < n;i++)
+	{
+		if (HT[i].parent = 0)
+		{
+			j = i;
+			k++;
+		}
+	}
 	while (!fin.eof())  //读到文件未
 	{
-		fin >> c;
-
-		if (HT[f].lch == 0 && HT[f].rch == 0)         //到达叶子结点时输出字符
+		f = j;                              //从根结点开始
+		//fin >> c;
+		while (HT[f].lch != 0 && HT[f].rch != 0 && !fin.eof())
+		//if ()         //到达叶子结点时输出字符
 		{
-			char ch = f + 96;
-			fout << ch;
-			cout << c;                         //显示结果
-			f = 2 * n - 1;                              //从根结点开始
+			fin.get(c);
+			if (c == '0')                              //当c为0是代表c为f的左孩子
+				f = HT[f].lch;
+			else
+				f = HT[f].rch;
+			
 		}
-		if (c == '0')                              //当c为0是代表c为f的左孩子
-			f = HT[f].lch;
-		else
-			f = HT[f].rch;
+		fout << HT[f].value;                       //显示结果
 	}
+	fin.close();
+	fout.close();
 }
 
 int main()
@@ -208,8 +250,10 @@ int main()
 	k = count_not_zero(data, N);
 	j = 2 * k - 1;
 	HT = (HuffmanTree)malloc(j*sizeof(HTNode));
-	CreatHuffmanTree(HT, j, data);//有k个字符,哈夫曼树有j个节点。
+	CreatHuffmanTree(HT, k, data);//有k个字符,哈夫曼树有j个节点。
 	HuffmanCode HC;
 	HC = (HuffmanCode)malloc(k*sizeof(HTNode));
-	CreatHuffmanCode(HT, HC, j);
+	CreatHuffmanCode(HT, HC, k);
+	Tranlate_To_HuffmanCode_File(HC, k);
+	TranslateHuffmanCode(HT, HC, j);
 }
